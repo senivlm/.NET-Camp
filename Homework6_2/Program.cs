b2edc9c2ed0232205ee4a6bbe051d5f2c33b3198
DESKTOP-H55BEMD\Dima <sobramenko@gmail.com>
2022-06-06 14:18:44 +0300

    Added homework 6_2

---
+++ Homework6_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Homework6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkWithTextFromFile workWithText = new WorkWithTextFromFile("text.txt");
+            Console.WriteLine(workWithText);
+            workWithText.WriteEachSentenceToFile();
+            workWithText.GetLongestWordsFromEachSentence();
+            Console.WriteLine("The longest words in each sentence");
+            string[] longestWords = workWithText.GetLongestWordsFromEachSentence();
+            for (int i = 0; i < longestWords.Length; i++)
+            {
+                Console.WriteLine(longestWords[i]);
+            }
+            Console.WriteLine("The smallest words in each sentence");
+            string[] smallestWords = workWithText.GetSmallestWordsFromEachSentence();
+            for (int i = 0; i < smallestWords.Length; i++)
+            {
+                Console.WriteLine(smallestWords[i]);
+            }
+        }
+    }
+}
---
+++ Homework6_2/WorkWithTextFromFile.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Homework6_2
+{
+    class WorkWithTextFromFile
+    {
+        private string text;
+        private string[] sentences;
+
+        public WorkWithTextFromFile(string fileName)
+        {
+            using (StreamReader reader = new StreamReader("text.txt"))
+            {
+                text = reader.ReadToEnd();
+            }
+        }
+
+        #region Methods
+
+        public void WriteEachSentenceToFile()
+        {
+            sentences = Regex.Split(text, @"(?<=[\.!\?])\s+");
+            using (StreamWriter writer = new StreamWriter("Result.txt"))
+            {
+                foreach (var item in sentences)
+                {
+                    writer.WriteLine(item);
+                }
+            }
+        }
+
+        public string[] GetLongestWordsFromEachSentence()
+        {
+            string[] longestWords = new string[sentences.Length];
+            char[] separators = { ' ', ',', '.', '!', '?', '\"', '\'', ':', '|'};
+            for (int i = 0; i < longestWords.Length; i++)
+            {
+                string[] words = sentences[i].Split(separators);
+                longestWords[i] = words[0];
+                for (int j = 1; j < words.Length; j++)
+                {
+                    if(words[j].Length > longestWords[i].Length)
+                    {
+                        longestWords[i] = words[j];
+                    }
+                }
+            }
+            return longestWords;
+        }
+
+        public string[] GetSmallestWordsFromEachSentence()
+        {
+            string[] smallestWords = new string[sentences.Length];
+            char[] separators = { ' ', ',', '.', '!', '?', '\"', '\'', ':', '|' };
+            for (int i = 0; i < smallestWords.Length; i++)
+            {
+                string[] words = sentences[i].Split(separators);
+                smallestWords[i] = words[0];
+                for (int j = 1; j < words.Length; j++)
+                {
+                    if (words[j].Length < smallestWords[i].Length && words[j].Length!= 0)
+                    {
+                        smallestWords[i] = words[j];
+                    }
+                }
+            }
+            return smallestWords;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return text;
+        }
+
+        #endregion
+    }
+}
