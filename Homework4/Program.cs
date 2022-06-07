12b9eee717e23e4e71cb428dede1389863741c9f
senivlm <91743906+senivlm@users.noreply.github.com>
2022-06-07 11:26:33 +0300

    Update Program.cs

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -9,7 +9,7 @@ namespace Homework4
             int[] arr = new int[] { 3, 100, 6, 2, 7, 1, 4, 8, 10, 4, 4, 99, 9 };
             Vector vector = new Vector(arr);
             Console.WriteLine(vector);
-
+//Не побачила реалізації для великого файлу
             //vector.QuickSort(0, arr.Length - 1);
             vector.SplitMergeSort(0, arr.Length);
             Console.WriteLine(vector);
